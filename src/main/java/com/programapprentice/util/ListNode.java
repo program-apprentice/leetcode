f2da4f661d99a79f7f1afea48b694fee1dbc5202
program-apprentice <rui.jiang.tech@gmail.com>
2015-08-04 17:04:23 -0700

    Added definition for singly-linked list ListNode

---
+++ src/main/java/com/programapprentice/util/ListNode.java
@@ -0,0 +1,13 @@
+package com.programapprentice.util;
+
+/**
+ * User: program apprentice
+ * Date: 8/4/15
+ * Time: 4:58 PM
+ */
+/** Definition for singly-linked list.*/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
