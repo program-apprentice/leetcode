2474e0fdf0f1a0849183a6aea77414896c4402c2
program-apprentice <rui.jiang.tech@gmail.com>
2015-08-09 12:17:48 -0700

    Add definition of Tree Node

---
+++ src/main/java/com/programapprentice/util/TreeNode.java
@@ -0,0 +1,13 @@
+package com.programapprentice.util;
+
+/**
+ * User: program-apprentice
+ * Date: 8/9/15
+ * Time: 12:14 PM
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
