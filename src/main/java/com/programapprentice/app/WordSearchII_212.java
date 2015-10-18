fe09fc8c26a23b6c59d368263667db8f5a281f73
program-apprentice <rui.jiang.tech@gmail.com>
2015-10-17 20:51:14 -0700

    Add unoptimized solution for 212 Word Search II

--- src/main/java/com/programapprentice/app/WordSearchII_212.java
+++ src/main/java/com/programapprentice/app/WordSearchII_212.java
@@ -10,6 +10,71 @@ import java.util.List;
  * Time: 3:36 PM
  */
 public class WordSearchII_212 {
+    public boolean exist(char[][] board, int m, int n, String word, int start, boolean[][] rec) {
+        if (m == board.length || n == board[0].length || m < 0 || n < 0 || word == null || word.isEmpty()) {
+            return false;
+        }
+        if(rec[m][n]) {
+            return false;
+        }
+        rec[m][n] = true;
+        if(board[m][n] != word.charAt(start)) {
+            rec[m][n] = false;
+            return false;
+        }
+        if(board[m][n] == word.charAt(start) && start == word.length()-1) {
+            return true;
+        }
+
+        if(exist(board, m-1, n, word, start+1, rec)) {
+            return true;
+        }
+        if(exist(board, m+1, n, word, start+1, rec)) {
+            return true;
+        }
+        if(exist(board, m, n-1, word, start+1, rec)) {
+            return true;
+        }
+        if(exist(board, m, n+1, word, start+1, rec)) {
+            return true;
+        }
+        rec[m][n] = false;
+        return false;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        if(board == null || board.length == 0 || board[0].length==0 || word == null || word.isEmpty()) {
+            return false;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] rec = new boolean[m][n];
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(exist(board, i, j, word, 0, rec)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public List<String> findWordsTooSlow(char[][] board, String[] words) {
+        if(board == null || board.length == 0 || board[0].length == 0) {
+            return new ArrayList<String>();
+        }
+
+        List<String> output = new ArrayList<String>();
+        for(String word : words) {
+            if(exist(board, word)) {
+                output.add(word);
+            }
+        }
+        return output;
+    }
+
+
+    /******************** Optimized Solution *******************************/
     private static final int R = 26;
     class TrieNode {
         boolean isEndOfWord;
